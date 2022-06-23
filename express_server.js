6bec0d83be2f68deb27286012f6ac09deea8eee5
Harinder Labana <sunny.labana@gmail.com>
2022-06-23 00:59:46 -0400

    Added a handler for the /hello path that resolves/passes HTML content

--- express_server.js
+++ express_server.js
@@ -14,8 +14,14 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
+//regester a handler for the root "/urls.json" path
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
+});
+
+//regester a handler for the root "/hello" path
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>');
 });
 
 app.listen(PORT, () => {
