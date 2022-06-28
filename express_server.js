6139cbb7173b18702a9e86fbcc5b7ce78f6bddd6
Harinder Labana <sunny.labana@gmail.com>
2022-06-27 20:37:47 -0400

    added logic to handle emtpy fields for a new url and editing exsisting ones. code has been reorganized and refactored

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const PORT = 8080;
 
 //function to gernerate random alphanumerical string
-function generateRandomString(n) {
+const generateRandomString = (n) => {
   let randomString = '';
   let characters =
     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -15,15 +15,24 @@ function generateRandomString(n) {
     );
   }
   return randomString;
-}
+};
 
+//initial database
 let urlDatabase = {
   b2xVn2: 'http://www.lighthouselabs.ca',
   '9sm5xk': 'http://www.google.com',
 };
 
+/*********************
+MIDDLEWARE REQUESTS
+**********************/
+
 //middleware - body-parser
 app.use(bodyParser.urlencoded({extended: true}));
+
+/*********************
+GET REQUESTS
+**********************/
 
 //use ejs as template engine
 app.set('view engine', 'ejs');
@@ -33,53 +42,9 @@ app.get('/', (req, res) => {
   res.send('Hello!');
 });
 
-//handler will render the form
-app.get('/urls/new', (req, res) => {
-  res.render('urls_new');
-});
-
-//handler for the root "/urls" path
-app.get('/urls', (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  //express knows to look in the 'views' folder when acessing 'urls_index'  no need to show path
-  res.render('urls_index', templateVars);
-});
-
-//handler that will assign a randomly generated shortURL to a longURL submission
-app.post('/urls', (req, res) => {
-  const shortURL = generateRandomString(6);
-  urlDatabase[shortURL] = req.body.longURL;
-  console.log(urlDatabase);
-  res.redirect(`/urls/${shortURL}`);
-});
-
-//handler to redirect from edit button to main list of urls
-app.post('/urls/:shortURL', (req, res) => {
-  urlDatabase[req.params.shortURL] = req.body.longURL;
-  res.redirect('/urls');
-});
-
-//handler to delete URL entries
-app.post('/urls/:shortURL/delete', (req, res) => {
-  const {shortURL} = req.params;
-  delete urlDatabase[shortURL];
-  res.redirect('/urls');
-});
-
-//handler for the root "/urls/:shortURL" path
-app.get('/urls/:shortURL', (req, res) => {
-  const templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL],
-  };
-  res.render('urls_show', templateVars);
-});
-
-//handler for redirection to longURL via shortURL link
-app.get('/u/:shortURL', (req, res) => {
-  const shortURL = Object.values(req.params);
-  const longURL = urlDatabase[shortURL];
-  res.redirect(longURL);
+//handler for the root "/hello" path
+app.get('/hello', (req, res) => {
+  res.send('<html><body>Hello <b>World</b></body></html>');
 });
 
 //handler for the root "/urls.json" path
@@ -87,10 +52,70 @@ app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
 });
 
-//handler for the root "/hello" path
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>');
+//handler for the list of urls
+app.get('/urls', (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
 });
+
+//handler for creating a new url
+app.get('/urls/new', (req, res) => {
+  res.render('urls_new');
+});
+
+//handler for the new shortURL
+app.get('/urls/:shortURL', (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  const shortURL = req.params.shortURL;
+  const templateVars = {
+    shortURL,
+    longURL,
+  };
+  res.render('urls_show', templateVars);
+});
+
+//handler for redirection to longURL via shortURL link
+app.get('/u/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];
+  res.redirect(longURL);
+});
+
+/*********************
+POST REQUESTS
+**********************/
+
+//handler that will assign a randomly generated shortURL to a longURL submission
+app.post('/urls', (req, res) => {
+  //check if it received
+  console.log(req.body);
+  if (req.body.longURL !== '') {
+    const shortURL = generateRandomString(6);
+    urlDatabase[shortURL] = req.body.longURL;
+    res.redirect(`/urls/${shortURL}`);
+  }
+});
+
+//handler to redirect from edit button to main list of urls
+app.post('/urls/:shortURL', (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = req.body.longURL;
+  if (longURL !== '') {
+    urlDatabase[shortURL] = longURL;
+    res.redirect('/urls');
+  }
+});
+
+//handler to delete URL entries
+app.post('/urls/:shortURL/delete', (req, res) => {
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL];
+  res.redirect('/urls');
+});
+
+/*********************
+EVENT LISTENERS
+**********************/
 
 //event listener showing server is awake and ready
 app.listen(PORT, () => {
